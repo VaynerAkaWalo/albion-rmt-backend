fa19925cff08bbfa3eb07d9436d988c64541abad
VaynerAkaWalo <vaynerakawalo@gmail.com>
2024-03-26 23:06:15 +0100

    Initial commit

---
+++ src/main/java/com/albionrmtempire/AlbionRmtBackendApplication.java
@@ -0,0 +1,13 @@
+package com.albionrmtempire;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbionRmtBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlbionRmtBackendApplication.class, args);
+    }
+
+}
