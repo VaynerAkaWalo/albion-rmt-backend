98b6a41c882dae2c6fe41b5c808ad23d6ba5ce04
VaynerAkaWalo <58806342+VaynerAkaWalo@users.noreply.github.com>
2024-04-07 21:19:24 +0200

    https setup (#15)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/zsh
 
 CONTAINER_NAME=albion-rmt-backend
+CONTAINER_KEY_STORE_LOCATION=/certs/albion-rmt.p12
 
 docker pull ghcr.io/vaynerakawalo/albion-rmt-backend:$1
 
@@ -8,4 +9,9 @@ docker kill $CONTAINER_NAME
 
 docker system prune -f
 
-docker run -dp 8050:8050 --name albion-rmt-backend  ghcr.io/vaynerakawalo/albion-rmt-backend:$1
+docker run -dp 8050:8050 \
+ -e KEY_STORE_LOCATION=$CONTAINER_KEY_STORE_LOCATION \
+ -e KEY_STORE_PASSWORD=$KEY_STORE_PASSWORD \
+ -v $KEY_STORE_LOCATION:$CONTAINER_KEY_STORE_LOCATION \
+ --name $CONTAINER_NAME \
+ ghcr.io/vaynerakawalo/albion-rmt-backend:$1
