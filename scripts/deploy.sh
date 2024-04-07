c63cd14003464f476bc0b44abd398a7826ef4acf
VaynerAkaWalo <58806342+VaynerAkaWalo@users.noreply.github.com>
2024-04-07 22:41:15 +0200

    change port (#17)

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/zsh
 
 CONTAINER_NAME=albion-rmt-backend
-CONTAINER_KEYSTORE_LOCATION=/certs/albion-rmt.p12
+CONTAINER_KEYSTORE_LOCATION=/certs/keystore.p12
 
 docker pull ghcr.io/vaynerakawalo/albion-rmt-backend:$1
 
@@ -9,9 +9,9 @@ docker kill $CONTAINER_NAME
 
 docker system prune -f
 
-docker run -dp 8050:8050 \
+docker run -dp 8443:8050 \
  -e KEYSTORE_LOCATION=$CONTAINER_KEYSTORE_LOCATION \
- -e KEYSTORE_PASSWORD=$KEY_STORE_PASSWORD \
- -v $KEYSTORE_LOCATION:$CONTAINER_KEYSTORE_LOCATION \
+ -e KEYSTORE_PASSWORD=$KEYSTORE_PASSWORD \
+ -v `dirname $KEYSTORE_LOCATION`:`dirname $CONTAINER_KEYSTORE_LOCATION` \
  --name $CONTAINER_NAME \
  ghcr.io/vaynerakawalo/albion-rmt-backend:$1
