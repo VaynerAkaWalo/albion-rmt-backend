f8d0aa02463f7e4931d4fdc65342df2b56fe438f
VaynerAkaWalo <58806342+VaynerAkaWalo@users.noreply.github.com>
2024-04-15 23:43:08 +0200

    Endpoint for publishing market offers, currently only black market offers are supported. (#22)

---
+++ src/main/java/com/albionrmtempire/dataobject/Order.java
@@ -0,0 +1,19 @@
+package com.albionrmtempire.dataobject;
+
+import com.albionrmtempire.datatransferobject.OrderRequest;
+
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+
+public record Order(
+        long totalPrice,
+        long amount,
+        short tier,
+        short enchant,
+        short quality,
+        boolean isFinished,
+        String buyer,
+        Item item,
+        ZonedDateTime acknowledgedDate,
+        ZonedDateTime expire
+) {}
--- src/main/java/com/albionrmtempire/exception/GlobalExceptionHandler.java
+++ src/main/java/com/albionrmtempire/exception/GlobalExceptionHandler.java
@@ -17,13 +17,23 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler
     ResponseEntity<ProblemDetail> handleGeneralException(Exception ex) {
-        return handleResponse(ex, "There was a problem try again later", HttpStatus.INTERNAL_SERVER_ERROR);
+        return handleResponse(ex, null, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler
+    ResponseEntity<ProblemDetail> handleNotFoundException(NotFoundException ex) {
+        return handleResponse(ex, ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    ResponseEntity<ProblemDetail> handleUnsupportedBuyerType(UnsupportedBuyerException ex) {
+        return handleResponse(ex, ex.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     private ResponseEntity<ProblemDetail> handleResponse(Exception ex, String customMessage, HttpStatusCode statusCode) {
         log.warn("Exception occurred during request processing: {} statusCode: {}", ex.getMessage(), statusCode.value());
         if (StringUtils.isEmpty(customMessage)) {
-            customMessage = ex.getMessage();
+            customMessage = "There was a problem try again later";
         }
 
         return new ResponseEntity<>(ProblemDetail.forStatusAndDetail(statusCode, customMessage), statusCode);
