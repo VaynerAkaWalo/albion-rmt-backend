9e10275c3fe8eb906257364f28d3dfae5f13ec19
VaynerAkaWalo <58806342+VaynerAkaWalo@users.noreply.github.com>
2024-04-16 20:37:40 +0200

    docker network (#26)

--- scripts/deployDB.sh
+++ scripts/deployDB.sh
@@ -1,11 +1,15 @@
 #!/bin/zsh
 
+docker network create albion-network
+
 docker run \
   --name postgres \
   -e POSTGRES_DB=albionrmt \
-  -e POSTGRES_USER="$DB_USER" \
-  -e POSTGRES_PASSWORD="$DB_PASSWORD" \
+  -e POSTGRES_USER=$DB_USER \
+  -e POSTGRES_PASSWORD=$DB_PASSWORD \
   -p 5432:5432 \
   -v /data:/var/lib/postgresql/data \
   -d \
   postgres
+
+  docker network connect albion-network postgres
