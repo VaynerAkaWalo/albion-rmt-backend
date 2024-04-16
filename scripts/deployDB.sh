e4515f0668ebaf9c710e27f9d21a96ad979fce92
VaynerAkaWalo <58806342+VaynerAkaWalo@users.noreply.github.com>
2024-04-16 20:11:53 +0200

    connection to postgres database (#25)

---
+++ scripts/deployDB.sh
@@ -0,0 +1,11 @@
+#!/bin/zsh
+
+docker run \
+  --name postgres \
+  -e POSTGRES_DB=albionrmt \
+  -e POSTGRES_USER="$DB_USER" \
+  -e POSTGRES_PASSWORD="$DB_PASSWORD" \
+  -p 5432:5432 \
+  -v /data:/var/lib/postgresql/data \
+  -d \
+  postgres
