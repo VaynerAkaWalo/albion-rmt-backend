1e8900396fb7972e0ade8e344c321ded24bcd4b5
VaynerAkaWalo <vaynerakawalo@gmail.com>
2024-04-16 20:37:19 +0200

    docker network

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,6 +12,10 @@ docker system prune -f
 docker run -dp 8443:8050 \
  -e KEYSTORE_LOCATION=$CONTAINER_KEYSTORE_LOCATION \
  -e KEYSTORE_PASSWORD=$KEYSTORE_PASSWORD \
+ -e DB_URL=$DB_URL \
+ -e DB_USER=$DB_USER \
+ -e DB_PASSWORD=$DB_PASSWORD \
  -v `dirname $KEYSTORE_LOCATION`:`dirname $CONTAINER_KEYSTORE_LOCATION` \
+ --network=albion-network \
  --name $CONTAINER_NAME \
  ghcr.io/vaynerakawalo/albion-rmt-backend:$1
