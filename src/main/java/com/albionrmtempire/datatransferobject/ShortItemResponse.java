2b5a8676a7b3662d6f3d98101a729888bdf1072b
VaynerAkaWalo <58806342+VaynerAkaWalo@users.noreply.github.com>
2024-04-21 17:51:35 +0200

    always include display name in ShortItemResponse (#45)

--- src/main/java/com/albionrmtempire/datatransferobject/ShortItemResponse.java
+++ src/main/java/com/albionrmtempire/datatransferobject/ShortItemResponse.java
@@ -1,10 +1,11 @@
 package com.albionrmtempire.datatransferobject;
 
 import com.albionrmtempire.dataobject.Item;
+import org.apache.commons.lang3.StringUtils;
 
 public record ShortItemResponse(String name, String displayName) {
 
     public static ShortItemResponse from(Item item) {
-        return new ShortItemResponse(item.systemName(), item.displayName());
+        return new ShortItemResponse(item.systemName(), StringUtils.isEmpty(item.displayName()) ? item.systemName() : item.displayName());
     }
 }
