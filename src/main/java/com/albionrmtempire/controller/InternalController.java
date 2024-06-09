af19234ba13c1bd2bac1adef332c1981aa044c95
VaynerAkaWalo <58806342+VaynerAkaWalo@users.noreply.github.com>
2024-06-09 18:36:43 +0200

    Scheduled match scraping (#63)

--- src/main/java/com/albionrmtempire/controller/InternalController.java
+++ src/main/java/com/albionrmtempire/controller/InternalController.java
@@ -1,6 +1,8 @@
 package com.albionrmtempire.controller;
 
+import com.albionrmtempire.provider.AlbionApiProvider;
 import com.albionrmtempire.repository.ItemOrderRepository;
+import com.albionrmtempire.service.crystalleague.CrystalMatchService;
 import io.swagger.v3.oas.annotations.Hidden;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -14,10 +16,19 @@ import org.springframework.web.bind.annotation.*;
 public class InternalController {
 
     private final ItemOrderRepository itemOrderRepository;
+    private final AlbionApiProvider albionApiProvider;
+    private final CrystalMatchService crystalMatchService;
 
     @DeleteMapping("orders")
     @ResponseStatus(HttpStatus.GONE)
     void dropOrders() {
         itemOrderRepository.deleteAll();
+    }
+
+    @GetMapping("fetchMatches")
+    void fetchAllCrystalMatches() {
+        final var matches = albionApiProvider.getAllMatches();
+
+        matches.forEach(crystalMatchService::persistMatchResults);
     }
 }
